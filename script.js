a53c86ed1a363a38ab5152fe763f5e05c56fa04a
Carol Peng <c.peng255@gmail.com>
2025-02-17 14:42:42 -0500

    semifinished

--- script.js
+++ script.js
@@ -12,7 +12,40 @@ function fullScreen() {
   } 
   
   let button = document.getElementById('full-screen');
-    button.style.display = 'none';
+  button.style.display = 'none';
+  
+  let container = document.getElementById('container');
+  container.style.display = 'block';
 } 
 
+document.addEventListener('fullscreenchange', function() {
+  if (!document.fullscreenElement) {
+    let container = document.getElementById('container');
+    container.style.display = 'none';
+    
+    let button = document.getElementById('full-screen');
+    button.style.display = 'block';
 
+    let nobutton = document.getElementById("no");
+    nobutton.style.left = "780px"; 
+    nobutton.style.top = "526px";
+  }
+});
+
+//function yes() {
+
+//}
+
+function no() {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const button = document.getElementById("no")
+  const buttonWidth = button.offsetWidth;
+  const buttonHeight = button.offsetHeight;
+
+  const x = Math.random() * (windowWidth - buttonWidth);
+  const y = Math.random() * (windowHeight - buttonHeight);
+
+  button.style.left = x + "px"; 
+  button.style.top = y + "px"; 
+}
